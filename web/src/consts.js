97556011375f1e69fe95f548297457eb68e3808e
sebascm-dev <spanolocabo@gmail.com>
2024-02-21 15:28:35 +0100

    first commit

---
+++ web/src/consts.js
@@ -0,0 +1,14 @@
+export const PROJECTS = [
+  {
+    slug: "01-arkanoid-game",
+    title: "Arkanoid Game",
+    description:
+      "Juego mítico y clásico de Arkanoid para controlar con teclado",
+    learnings: [
+      "Dibujar en Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites"
+    ]
+  },
+]
